d503262bc5378f0b9d8424c5682bcf2259edd72d
vunguyen12 <54542419+vunguyen12@users.noreply.github.com>
2019-08-28 09:30:49 -0500

    update newest work

---
+++ src/TableAndChairs.java
@@ -0,0 +1,22 @@
+public class TableAndChairs {
+    public static void main(String[] args) {
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("C");
+        System.out.println("SEATSEATSEAT                     TableTableTableTable");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+        System.out.println("L           L                    LL                LL");
+
+    }
+}
---
+++ src/TacoTruck.java
@@ -0,0 +1,15 @@
+public class TacoTruck {
+
+    public static void main(String[] args) {
+        System.out.println("I----------------------------------I");
+        System.out.println("I                            ------I");
+        System.out.println("I                            I     I");
+        System.out.println("I   definitely a taco truck  I____ I------I");
+        System.out.println("I                                     ___   I");
+        System.out.println("I                                    I   I  I");
+        System.out.println("I                                    I   I  I");
+        System.out.println("I---IllllllI--------------lllllllI---I---I--I");
+        System.out.println("    IllOllI               lllOlll");
+        System.out.println("    lllllll               lllllll");
+    }
+}
